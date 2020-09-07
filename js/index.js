62e5fced72b8d467b9bbf2579f5e66ee3f82aad1
Rahul Gupta <52673814+RAHUL1115@users.noreply.github.com>
2020-09-07 11:16:30 +0530

    -feature1

--- js/index.js
+++ js/index.js
@@ -22,3 +22,25 @@ function scrollToId(id){
         behavior: 'smooth'
     });
 }
+
+function menuclick() {
+    var box = document.getElementById('box');
+    var menu = document.getElementById('menubaritem');
+    if (box.getAttribute('value') == "true") {
+        console.log("if");
+        box.setAttribute("value", "false");
+        box.children[0].setAttribute("transform", "translate(-40, 20) rotate(45 60 60)");
+        box.children[1].setAttribute("width", "0");
+        box.children[1].setAttribute("x", "50");
+        box.children[2].setAttribute("transform", "translate(-5, -35) rotate(-45 60 60)");
+        menu.classList.toggle('hidden');
+    } else {
+        console.log("else");
+        box.setAttribute("value", "true");
+        box.children[0].removeAttribute("transform");
+        box.children[1].setAttribute("width", "100");
+        box.children[1].setAttribute("x", "0");
+        box.children[2].removeAttribute("transform");
+        menu.classList.toggle('hidden');
+    }
+}
