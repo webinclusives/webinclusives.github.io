16216ab955310761bd315b25ba5433ddec5c60f2
Rahul Gupta <52673814+RAHUL1115@users.noreply.github.com>
2020-09-08 11:33:06 +0530

    --simple and attractive

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ var w = window.innerWidth || document.documentElement.clientWidth || document.bo
 var h = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 
 window.onscroll = (e)=>{
-    if (document.body.scrollTop > h/10 || document.documentElement.scrollTop > h/10) {
+    if (document.body.scrollTop > h/20 || document.documentElement.scrollTop > h/20) {
         //after scroll
         document.getElementById("navbar").classList.add('shadow-lg');
     } else {
@@ -24,23 +24,9 @@ function scrollToId(id){
 }
 
 function menuclick() {
-    var box = document.getElementById('box');
-    var menu = document.getElementById('menubaritem');
-    if (box.getAttribute('value') == "true") {
-        console.log("if");
-        box.setAttribute("value", "false");
-        box.children[0].setAttribute("transform", "translate(-40, 20) rotate(45 60 60)");
-        box.children[1].setAttribute("width", "0");
-        box.children[1].setAttribute("x", "50");
-        box.children[2].setAttribute("transform", "translate(-5, -35) rotate(-45 60 60)");
-        menu.classList.toggle('hidden');
-    } else {
-        console.log("else");
-        box.setAttribute("value", "true");
-        box.children[0].removeAttribute("transform");
-        box.children[1].setAttribute("width", "100");
-        box.children[1].setAttribute("x", "0");
-        box.children[2].removeAttribute("transform");
-        menu.classList.toggle('hidden');
-    }
+    //other menu tasks
+    document.getElementById('menubaritem').classList.toggle('hidden');
+    //disbale scroll
+    document.getElementsByTagName("body")[0].classList.toggle('disabledcroll');
+    document.getElementsByTagName("html")[0].classList.toggle('disabledcroll');
 }
